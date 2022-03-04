7a8e5dd1b81295f1f16c34f1af79655c3036afac
acoathup <acoat00@gmail.com>
2022-03-04 11:54:11 +0100

    added script to have continuous video stream

---
+++ AndrewPymbaScript.py
@@ -0,0 +1,30 @@
+'''
+This is an example to stream video from the camera, it was taken from 
+https://github.com/morefigs/pymba/blob/master/examples/camera/opencv_acquire_streaming_images.py
+
+'''
+
+from time import sleep
+from pymba import Vimba
+from examples.camera._display_frame import display_frame
+
+
+if __name__ == '__main__':
+
+    with Vimba() as vimba:
+        camera = vimba.camera(0)
+        camera.open()
+
+        # arm the camera and provide a function to be called upon frame ready
+        camera.arm('Continuous', display_frame)
+        camera.start_frame_acquisition()
+
+        # stream images for a while...
+        sleep(10)
+
+        # stop frame acquisition
+        # start_frame_acquisition can simply be called again if the camera is still armed
+        camera.stop_frame_acquisition()
+        camera.disarm()
+
+        camera.close()
